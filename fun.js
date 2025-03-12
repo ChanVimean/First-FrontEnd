38092423e73dd15be35605729768a39819ee17e4
ChanVimean <mrrmeanvlogger@gmail.com>
2025-03-12 11:31:55 +0700

    Initial commit

---
+++ fun.js
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////
+// Duplicate Track Slider Animation
+// Section 4 Snake Slider
+var copyTrack = $('.track-slide').clone();
+$('.track').append(copyTrack)
+//////////////////////////////////////////////////////////
+// Video Slider
+const wrapper = document.querySelector('.wrapper');
+
+let pressed = false;
+let startX = 0;
+
+wrapper.addEventListener('mousedown', function(e){
+    pressed = true;
+    startX = e.clientX;
+    this.style.cursor = 'grabbing';
+});
+wrapper.addEventListener('mouseleave', function(e){
+    pressed = false;
+});
+window.addEventListener('mouseup', function(e){
+    pressed = false;
+    wrapper.style.cursor = 'grab';
+
+});
+wrapper.addEventListener('mousemove', function(e){
+    if(!pressed){
+        return 
+    }
+    this.scrollLeft += startX - e.clientX
+});
+
